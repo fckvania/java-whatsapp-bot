51c11adaac6ebbbd1826535a9306bec26482bb29
Vania <vaniaalexanakano@gmail.com>
2022-11-24 14:02:10 +0000

    perbaikan

--- src/main/java/mybot/maple/Maple.java
+++ src/main/java/mybot/maple/Maple.java
@@ -1,10 +1,7 @@
 package mybot.maple;
-
-import it.auties.whatsapp.api.ErrorHandler;
-import it.auties.whatsapp.api.HistoryLength;
+;
 import it.auties.whatsapp.api.QrHandler;
 import it.auties.whatsapp.api.Whatsapp;
-import it.auties.whatsapp.model.message.model.TextPreviewSetting;
 import it.auties.whatsapp.model.signal.auth.Version;
 import mybot.maple.message.Message;
 
@@ -13,16 +10,12 @@ import java.util.concurrent.ExecutionException;
 public class Maple {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var waOpts = Whatsapp.Options.newOptions()
-                .autodetectListeners(true)
-                .textPreviewSetting(TextPreviewSetting.ENABLED)
                 .version(new Version(2, 2244, 6))
                 .description("Maple")
-                .historyLength(HistoryLength.THREE_MONTHS)
-                .errorHandler(ErrorHandler.toFile())
                 .qrHandler(QrHandler.toTerminal())
                 .build();
         Whatsapp.lastConnection(waOpts)
-                .addLoggedInListener(() -> System.out.println("Bot Terkonek"))
+                .addLoggedInListener(() -> System.out.println("Connected :>"))
                 .addNewMessageListener(Message::onNewMessage)
                 .connect()
                 .get();
--- src/main/java/mybot/maple/lib/Functions.java
+++ src/main/java/mybot/maple/lib/Functions.java
@@ -26,31 +26,4 @@ public class Functions {
 
         return response.body();
     }
-
-    /**
-     *
-     * @param is
-     * @return
-     */
-    public String convertStreamToString(InputStream is) {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
 }
